4bf5a12e864124798869b0733e2f703b65feeea2
abreslav <abreslav@6fce7312-e72d-0410-9edc-b760b34c0948>
2007-04-18 06:36:40 +0000

    Code generation tested

--- trunk/ru.ifmo.rain.astrans.tests/src/ru/ifmo/rain/astrans/interpreter/backtrans/BacktransCodeGeneratorTest.java
+++ trunk/ru.ifmo.rain.astrans.tests/src/ru/ifmo/rain/astrans/interpreter/backtrans/BacktransCodeGeneratorTest.java
@@ -1,9 +1,15 @@
 package ru.ifmo.rain.astrans.interpreter.backtrans;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Collection;
 
+import org.eclipse.emf.codegen.util.ImportManager;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -47,6 +53,35 @@ public class BacktransCodeGeneratorTest {
 		Transformation transformation = (Transformation) transfomationResource.getContents().get(0);
 		
 		BacktransCodeGenerator.generate(transformation, "ru.astrans", dataDir);
+		
+		assertEqualFiles(dataDir + "/expected.java", dataDir + "/" + transformation.getName() + ".java");
+		assertEqualFiles(dataDir + "/expectedTrace.java", dataDir + "/" + transformation.getTraceClassName().getName() + ".java");
+		assertEqualFiles(dataDir + "/expectedResolver.java", dataDir + "/" + transformation.getResolverClassName().getName() + ".java");
 	}
-	
+
+	@Test
+	public void testIM() {
+		ImportManager manager = new ImportManager("ru");
+		manager.addImport("ru.amse.SomeClass");
+		manager.addImport("SomeClass");
+		assertEquals("SomeClass", manager.getImportedName("SomeClass"));
+		assertEquals("SomeClass", manager.getImportedName("ru.amse.SomeClass"));
+	}
+
+	private static void assertEqualFiles(String expectedFile, String resultFile) throws FileNotFoundException, IOException {
+		Reader expectedTransformation = new FileReader(expectedFile);
+		Reader result = new FileReader(resultFile);
+		int i = 0;
+		int resultC;
+		int expectedC;
+		do {
+			resultC = result.read();
+			expectedC = expectedTransformation.read();
+			assertEquals("transformation" + ": " + i, expectedC, resultC);
+			if (expectedC == -1 || resultC == -1) {
+				break;
+			}
+			i++;
+		} while (expectedC == -1 || resultC == -1);
+	}
 }
