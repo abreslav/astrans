a11afce3a1fe7906dfc1ddc91ba41419bc1862ba
abreslav <abreslav@6fce7312-e72d-0410-9edc-b760b34c0948>
2007-04-18 09:25:14 +0000

    Creator implemented Basic typed supported

--- trunk/ru.ifmo.rain.astrans.backtrans/src/ru/ifmo/rain/astrans/interpreter/backtrans/BacktransCreator.java
+++ trunk/ru.ifmo.rain.astrans.backtrans/src/ru/ifmo/rain/astrans/interpreter/backtrans/BacktransCreator.java
@@ -2,19 +2,34 @@ package ru.ifmo.rain.astrans.interpreter.backtrans;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
+import org.eclipse.emf.codegen.ecore.genmodel.GenClass;
+import org.eclipse.emf.codegen.ecore.genmodel.GenClassifier;
+import org.eclipse.emf.codegen.ecore.genmodel.GenDataType;
+import org.eclipse.emf.codegen.ecore.genmodel.GenFeature;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
 import org.eclipse.emf.codegen.util.CodeGenUtil;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EEnumLiteral;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
 
 import ru.ifmo.rain.astrans.astransformation.AssignAttribute;
+import ru.ifmo.rain.astrans.astransformation.AssignFeature;
 import ru.ifmo.rain.astrans.astransformation.AssignReference;
 import ru.ifmo.rain.astrans.astransformation.AstransformationFactory;
+import ru.ifmo.rain.astrans.astransformation.AstransformationPackage;
+import ru.ifmo.rain.astrans.astransformation.BasicType;
+import ru.ifmo.rain.astrans.astransformation.BasicTypeName;
+import ru.ifmo.rain.astrans.astransformation.ClassName;
 import ru.ifmo.rain.astrans.astransformation.MappingRule;
 import ru.ifmo.rain.astrans.astransformation.ResolveObject;
 import ru.ifmo.rain.astrans.astransformation.Transformation;
+import ru.ifmo.rain.astrans.astransformation.TypeName;
 import ru.ifmo.rain.astrans.trace.ClassMapping;
 import ru.ifmo.rain.astrans.trace.ReferenceMapping;
 import ru.ifmo.rain.astrans.trace.ReferenceMappingType;
@@ -28,19 +43,23 @@ public class BacktransCreator {
 		backTransformation.setName(name + "Transformation");
 		backTransformation.setResolverClassName("I" + name + "Resolver");
 		backTransformation.setTraceClassName("I" + name + "Trace");
+		backTransformation.setSwitchClassName(createClassName(imageGM.getSwitchClassName()));
 	
 		Collection<ClassMapping> mappings = trace.getClassMappings();
 		for (ClassMapping mapping : mappings) {
 			MappingRule rule = AstransformationFactory.eINSTANCE.createMappingRule();
 			backTransformation.getMappingRules().add(rule);
 			rule.setName("case" + mapping.getImage().getName());
+			rule.setFactoryClassName(createClassName(protoGM.getFactoryClassName()));
+			rule.setFactoryMethodName("create" + mapping.getProto().getName()); 
 
 			rule.setResult(AstransformationFactory.eINSTANCE.createParameter());
 			rule.getResult().setName(lowercaseFirstLetter(mapping.getProto().getName()));
-			rule.getResult().setType(mapping.getProto().getName());
+			rule.getResult().setType((ClassName) getTypeName(protoGM, mapping.getProto()));
+
 			
 			rule.setParameter(AstransformationFactory.eINSTANCE.createParameter());
-			rule.getParameter().setType(mapping.getImage().getName());
+			rule.getParameter().setType((ClassName) getTypeName(imageGM, mapping.getImage()));
 			rule.getParameter().setName(lowercaseFirstLetter(mapping.getImage().getName()));
 			
 			rule.setWriteTraceStatement(AstransformationFactory.eINSTANCE.createWriteTrace());
@@ -54,8 +73,10 @@ public class BacktransCreator {
 			for (Iterator iter = allAttributes.iterator(); iter.hasNext();) {
 				EAttribute attribute = (EAttribute) iter.next();
 				AssignAttribute assignAttribute = AstransformationFactory.eINSTANCE.createAssignAttribute();
-				assignAttribute.setSetterName(attribute.getName());
-				assignAttribute.setGetterName(trace.getAttributeMapping(attribute).getImage().getName());
+				EAttribute imageAttribute = trace.getAttributeMapping(attribute).getImage();
+
+				initAssignFeature(assignAttribute, attribute, imageAttribute, protoGM, imageGM);
+				
 				rule.getAssignAttributeStatements().add(assignAttribute);
 			}
 			
@@ -63,16 +84,19 @@ public class BacktransCreator {
 			for (Iterator iter = allReferences.iterator(); iter.hasNext();) {
 				EReference reference = (EReference) iter.next();
 				ReferenceMapping referenceMapping = trace.getReferenceMapping(reference);
+				EStructuralFeature imageReference = referenceMapping.getImage();
 				if (referenceMapping.getType() == ReferenceMappingType.TRANSLATED_LITERAL) {
 					ResolveObject resolveObject = AstransformationFactory.eINSTANCE.createResolveObject();
-					resolveObject.setDest(reference);
-					resolveObject.setSource(referenceMapping.getImage());
+					initAssignFeature(resolveObject, reference, imageReference, protoGM, imageGM);
 					resolveObject.setResolverMethodName("resolve" + mapping.getProto().getName() + uppercaseFirstLetter(reference.getName()));
+					
+					resolveObject.setParameterName(lowercaseFirstLetter(imageReference.getEType().getName()));
+					resolveObject.setParameterType(getTypeName(imageGM, imageReference.getEType()));
+					resolveObject.setType((ClassName) getTypeName(protoGM, reference.getEReferenceType()));
 					rule.getResolveObjectStatements().add(resolveObject);
 				} else {
 					AssignReference assignReference = AstransformationFactory.eINSTANCE.createAssignReference();
-					assignReference.setDest(reference);
-					assignReference.setSource((EReference) referenceMapping.getImage());
+					initAssignFeature(assignReference, reference, imageReference, protoGM, imageGM);
 					assignReference.setMappingNeeded(referenceMapping.getType() != ReferenceMappingType.NONE_LITERAL);
 					rule.getAssignReferenceStatements().add(assignReference);
 				}
@@ -82,13 +106,99 @@ public class BacktransCreator {
 		return backTransformation;
 	}
 
+	private static String getSetterName(GenFeature genFeature) {
+		return genFeature.getEcoreFeature().isMany() 
+		? genFeature.getGetAccessor()
+		: "set" + genFeature.getAccessorName();
+	}
+
+	private static GenFeature getGenFeature(GenPackage genPackage, EStructuralFeature feature) {
+		GenClassifier genClassifier = getGenClassifier(genPackage, feature.getEContainingClass());
+		if (genClassifier instanceof GenClass == false) {
+			throw new IllegalArgumentException("GenClass not found for " + feature.getEContainingClass().getName());
+		}
+		GenClass genClass = (GenClass) genClassifier;
+		
+		List allGenFeatures = genClass.getAllGenFeatures();
+		for (Iterator iter = allGenFeatures.iterator(); iter.hasNext();) {
+			GenFeature genFeature = (GenFeature) iter.next();
+			if (genFeature.getEcoreFeature() == feature) {
+				return genFeature;
+			}
+		}
+		throw new IllegalArgumentException("GenFeature not found for " + feature.getEContainingClass().getName() + "." + feature.getName());
+	}
+
+	private static TypeName getTypeName(GenPackage genPackage, EClassifier eClassifier) {
+		GenClassifier genClassifier = getGenClassifier(genPackage, eClassifier);
+		if (genClassifier == null) {
+			throw new IllegalArgumentException("GenClass not found for " + eClassifier.getName());
+		}
+		if (genClassifier instanceof GenClass) {
+			GenClass genClass = (GenClass) genClassifier;
+			return createClassName(genClass.getQualifiedInterfaceName());
+		}
+		if (genClassifier instanceof GenDataType) {
+			GenDataType genDataType = (GenDataType) genClassifier;
+			String name = genDataType.getQualifiedInstanceClassName();
+			
+			EEnumLiteral enumLiteral = AstransformationPackage.eINSTANCE.getBasicType().getEEnumLiteral(name);
+			if (enumLiteral != null) {
+				BasicTypeName typeName = AstransformationFactory.eINSTANCE.createBasicTypeName();
+				typeName.setType((BasicType) enumLiteral.getInstance());
+			}
+			return createClassName(name);
+		}
+		throw new IllegalStateException();
+	}
+
+	private static GenClassifier getGenClassifier(GenPackage genPackage, EClassifier eClass) {
+		GenClassifier found = lookupGenClass(genPackage, eClass);
+		if (found == null) {
+			GenModel genModel = genPackage.getGenModel();
+			EList genPackages = genModel.getGenPackages();
+			for (Iterator iter = genPackages.iterator(); iter.hasNext();) {
+				GenPackage gPackage = (GenPackage) iter.next();
+				if (gPackage != genPackage) {
+					found = lookupGenClass(gPackage, eClass);
+					if (found != null) {
+						break;
+					}
+				}
+			}
+		}
+		return found;
+	}
+
+	private static GenClassifier lookupGenClass(GenPackage genPackage, EClassifier eClassifier) {
+		EList genClassifiers = genPackage.getGenClassifiers();
+		for (Iterator iter = genClassifiers.iterator(); iter.hasNext();) {
+			GenClassifier genClassifier = (GenClassifier) iter.next();
+			if (genClassifier.getEcoreClassifier() == eClassifier) {
+				return genClassifier;
+			}
+		}
+		return null;
+	}
+	
+	private static void initAssignFeature(AssignFeature assignFeature, EStructuralFeature proto, EStructuralFeature image, GenPackage protoGM, GenPackage imageGM) {
+		assignFeature.setSetterName(getSetterName(getGenFeature(protoGM, proto)));
+		assignFeature.setGetterName(getGenFeature(imageGM, image).getGetAccessor());
+		
+	}
+	
+	private static ClassName createClassName(String className) {
+		ClassName result = AstransformationFactory.eINSTANCE.createClassName();
+		result.setName(className);
+		return result;
+	}
+
 	private static String lowercaseFirstLetter(String name) {
-//		return Character.toLowerCase(name.charAt(0)) + name.substring(1);
 		return CodeGenUtil.uncapName(name);
 	}
 	
 	private static String uppercaseFirstLetter(String name) {
-//		return Character.toUpperCase(name.charAt(0)) + name.substring(1);
 		return CodeGenUtil.capName(name);
 	}
 }
+ 
