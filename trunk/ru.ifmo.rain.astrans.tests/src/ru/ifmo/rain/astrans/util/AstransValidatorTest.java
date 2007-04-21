0ec12ca3fd25becc35e3e16c40ec3a681ab85c71
abreslav <abreslav@6fce7312-e72d-0410-9edc-b760b34c0948>
2007-04-21 14:40:15 +0000

    PrototypeIsNotSkipped constraint is tested

--- trunk/ru.ifmo.rain.astrans.tests/src/ru/ifmo/rain/astrans/util/AstransValidatorTest.java
+++ trunk/ru.ifmo.rain.astrans.tests/src/ru/ifmo/rain/astrans/util/AstransValidatorTest.java
@@ -2,8 +2,10 @@ package ru.ifmo.rain.astrans.util;
 
 import static org.junit.Assert.assertFalse;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -16,18 +18,43 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EValidator.SubstitutionLabelProvider;
 import org.eclipse.emf.ecore.util.Diagnostician;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import ru.ifmo.rain.astrans.AstransPackage;
 import ru.ifmo.rain.astrans.utils.EMFHelper;
 import utils.FileUtils;
+import utils.IFileProcessor;
 
+@RunWith(Parameterized.class)
 public class AstransValidatorTest {
+
+	@Parameters
+	public static Collection<Object[]> parameters() {
+		return FileUtils.processDirectory("testData/modelvalidation", new IFileProcessor() {
+
+			public Object[] process(File file) {
+				return new Object[] {
+						file
+				};
+			}
+			
+		});
+	}
+	
+	private final File testDir;
+	
+	
+	public AstransValidatorTest(final File testDir) {
+		super();
+		this.testDir = testDir;
+	}
 
 	@SuppressWarnings("unchecked")
 	@Test
-	public final void testValidateSkipClass_FullyDefineAbstractClasses() throws IOException {
-		String dir = "testData/modelvalidation/testFullyDefineAbstractClasses/";
-		EObject object = EMFHelper.loadEObjectFromXMIResource(AstransPackage.eINSTANCE, dir + "Transformation.xmi");
+	public final void testConstraints() throws IOException {
+		EObject object = EMFHelper.loadEObjectFromXMIResource(AstransPackage.eINSTANCE, new File( testDir, "Transformation.xmi").getPath());
 		BasicDiagnostic diagnostics = new BasicDiagnostic(AstransValidator.DIAGNOSTIC_SOURCE, 0, "", new Object[] {});
 		Map context = new HashMap();
 		SubstitutionLabelProvider labelProvider = new SubstitutionLabelProvider() {
@@ -49,14 +76,14 @@ public class AstransValidatorTest {
 		boolean b = Diagnostician.INSTANCE.validate(object, diagnostics, context);
 		assertFalse(b);
 		
-		FileWriter messages = new FileWriter(dir + "messages");
+		FileWriter messages = new FileWriter(new File(testDir, "messages"));
 		for (Iterator iter = diagnostics.getChildren().iterator(); iter.hasNext();) {
 			Diagnostic diagnostic = (Diagnostic) iter.next();
 			messages.write(diagnostic.getMessage() + "\n");
 		}
 		messages.close();
 		
-		FileUtils.assertEqualFiles("", dir + "expected_messages", dir + "messages");
+		FileUtils.assertEqualFiles("", new File(testDir, "expected_messages").getPath(), new File(testDir, "messages").getPath());
 	}
 
 }
